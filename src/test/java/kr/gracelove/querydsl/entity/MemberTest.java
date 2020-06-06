87b78db4fb5a4bded08deffd4b6267424c1e4a60
gracelove <govlmo91@gmail.com>
2020-06-07 06:05:30 +0900

    정렬

--- src/test/java/kr/gracelove/querydsl/entity/MemberTest.java
+++ src/test/java/kr/gracelove/querydsl/entity/MemberTest.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 import static kr.gracelove.querydsl.entity.QMember.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 /**
  * Created by GraceLove
@@ -145,7 +146,37 @@ class MemberTest {
         long fetchCount = queryFactory
                 .selectFrom(member)
                 .fetchCount();// count 쿼리나감.
+    }
 
+
+    /**
+     * 회원 정렬순서.
+     * 1. 회원 나이 내림차순 (desc)
+     * 2. 회원 이름 올림차순(asc)
+     * 단 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+     */
+    @Test
+    void sort() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(
+                        member.age.desc(),
+                        member.username.asc().nullsLast()
+                )
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+        assertEquals("member5", member5.getUsername());
+        assertEquals("member6", member6.getUsername());
+        assertNull(memberNull.getUsername());
 
     }
 
